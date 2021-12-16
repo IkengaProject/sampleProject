eb30a44d769e9fb787634be1a2a765268d48c45e
Animecrazy13 <jzhasreeljosephbondoc@gmail.com>
2021-12-15 22:24:20 -0800

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ console.log("hi")
 let myFunc = function () {
   console.log("Manny and Joseph's favorite anime is bok no pico part one and two")
 }
+console.log("Peyton loves boku no pico too. Don't worry, we dont judge.") 
